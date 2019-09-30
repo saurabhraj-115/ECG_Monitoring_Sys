a4bd5c9117501b5042d1ca22e980c7adc1a0b4c2
Saurabh Raj <saurabhraj115@gmail.com>
2019-09-30 18:24:53 +0530

    New

--- arduino_raspi/arduino_raspi.ino
+++ arduino_raspi/arduino_raspi.ino
@@ -1,17 +1,19 @@
 void setup() {
   Serial.begin(9600);
   pinMode(A0, INPUT);
-  pinMode(7, OUTPUT);
+  
 }
  
 void loop() {
   // put your main code here, to run repeatedly:
   int sensorValue= analogRead(A0);
-  /*float voltage= sensorValue * (5.0 / 1023.0);*/
   float voltage= sensorValue * (3.3 / 1023.0);
+/*float voltage= sensorValue * (3.3 / 1023.0);*/
+  delay(5);
   Serial.println(voltage);
+  
   /*Serial.println(sensorValue);*/
-  digitalWrite(7, voltage);
+  /*digitalWrite(7, voltage);*/
   /*Serial.write(sensorValue);*/
   /*Serial.write(sensorValue);*/
 
--- final.py
+++ final.py
@@ -3,13 +3,14 @@ from os import system
 
 
 
-
+system('clear')
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 while 1: 
 	try:
 		if(ser.in_waiting >0):
 			line = ser.readline()
+			print line
 			cmd= "cd /home/pi/Desktop/aws-iot-device-sdk-python/samples/basicPubSub && sudo python basicPubSub.py -e a22m0pb0aki0bc-ats.iot.us-east-1.amazonaws.com -r root-CA.crt -c ECG_iot_RPi.cert.pem -k ECG_iot_RPi.private.key -t topic_1 -M "+ str(line)
 			system(cmd)
 	except KeyboardInterrupt:
